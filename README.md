14a7137b8f89d2ba9c21884257236244c4632f73
Marcelo Almeida <marcelo.almeida@jumia.com>
2017-11-14 03:02:54 +0000

    First commit

---
+++ README.md
@@ -0,0 +1,33 @@
+# scraperexporter
+
+Simple webserver that scrapes sites on configuration, filter the retrieved data and export them on Prometheus format
+
+[![Build Status](https://travis-ci.org/marcelosousaalmeida/scraperexporter.svg?branch=master)](https://travis-ci.org/marcelosousaalmeida/scraperexporter)
+
+## Installation
+
+If you are using Go 1.6+ (or 1.5 with the `GO15VENDOREXPERIMENT=1` environment variable), you can install `scraperexporter` with the following command:
+
+```bash
+$ go get -u github.com/marcelosousaalmeida/scraperexporter
+```
+
+## Usage
+
+```bash
+$ ./scraperexporter --help
+Usage of ./scraperexporter:
+  -configuration-file string
+    	Configuration file (default "conf.json")
+  -generate-config
+    	Generate configuration example file
+
+$ ./scraperexporter
+```
+## Contributing
+
+All contributions are welcome, but if you are considering significant changes, please open an issue beforehand and discuss it with us.
+
+## License
+
+MIT. See the `LICENSE` file for more information.
---
+++ main.go
@@ -0,0 +1,167 @@
+package main
+
+import (
+	"net/http"
+	"io/ioutil"
+	"fmt"
+	"log"
+	"time"
+	"strconv"
+	"net/url"
+	"regexp"
+	"os"
+	"encoding/json"
+	"gopkg.in/tylerb/graceful.v1"
+	"bytes"
+	"io"
+	"flag"
+	"sync"
+)
+
+type Configuration struct {
+	Sites []string
+	MetricString string
+	AvoidCache string
+	Path string
+	ListenerAddress string
+	RegexpString string
+}
+
+func getHttpBody(url string) string {
+	request, err := http.NewRequest("GET", url, nil)
+
+	client := &http.Client{}
+	resp, err := client.Do(request)
+
+	if err != nil {
+		log.Panicf("Error making HTTP request: %s", err)
+		return ""
+	}
+
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.Panicf("Error reading HTTP request body: %s", err)
+		return ""
+	}
+
+	return string(body)
+}
+
+func buildURL(urlRaw string, avoidCache ...string) string {
+	u, err  := url.Parse(urlRaw)
+
+	if err != nil {
+		log.Fatalf("Error building URL: %s", err)
+	}
+
+	if len(avoidCache) > 0 && avoidCache[0] == "true" {
+		queryString := strconv.FormatInt(time.Now().Unix(), 10)
+		q := u.Query()
+		q.Set("z" + queryString, queryString)
+		u.RawQuery = q.Encode()
+	}
+
+	return u.String()
+}
+
+func extractValue(bodyRaw string, regexString string) string {
+	if bodyRaw == "" {
+		return "0"
+	} else {
+		re := regexp.MustCompile(regexString)
+		regexpResult := re.FindAllStringSubmatch(bodyRaw, -1)
+
+		return regexpResult[0][1]
+	}
+}
+
+func buildSingleResultLine(fullyUrl string, value string, custom_metric_string string) string {
+	if custom_metric_string == "" {
+		custom_metric_string = "extracted_value"
+	}
+
+	return custom_metric_string + "{url=\"" + fullyUrl + "\"} " + value
+}
+
+func readConfiguration(filename string) Configuration {
+	file, _ := os.Open(filename)
+	decoder := json.NewDecoder(file)
+	configuration := Configuration{}
+	err := decoder.Decode(&configuration)
+	if err != nil {
+		log.Fatalf("Error reading configuration file: '%s'", err)
+	}
+
+	return configuration
+}
+
+func buildConfigExample() *bytes.Buffer {
+	configuration := Configuration{}
+
+	configuration.Sites = append(configuration.Sites, "https://www.jumia.com.ng")
+	configuration.Sites = append(configuration.Sites, "https://www.jumia.com.eg")
+	configuration.MetricString = "value"
+	configuration.AvoidCache = "true"
+	configuration.Path = "/products/"
+	configuration.ListenerAddress = "0.0.0.0:8080"
+	configuration.RegexpString = "(.*)"
+
+	buf := &bytes.Buffer{}
+
+	jsonObject := json.NewEncoder(buf)
+	jsonObject.SetIndent("", "  ")
+	jsonObject.Encode(configuration)
+
+	return buf
+}
+
+func main() {
+
+	configFile := flag.String("configuration-file", "conf.json", "Configuration file")
+	generateConfigFile := flag.Bool("generate-config", false, "Generate configuration example file")
+	flag.Parse()
+
+	// @todo - not working as expected
+	if *generateConfigFile == true {
+		fmt.Println(buildConfigExample())
+		os.Exit(0)
+	}
+
+	config := readConfiguration(*configFile)
+
+	var wg sync.WaitGroup
+
+	server := &graceful.Server{
+		Timeout: 10 * time.Second,
+		Server: &http.Server{
+			Addr: config.ListenerAddress,
+			ReadTimeout: time.Duration(5) * time.Second,
+			Handler: http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+				buf := &bytes.Buffer{}
+
+				wg.Add(len(config.Sites))
+				for _, site := range config.Sites {
+					go func(site string) {
+						defer wg.Done()
+
+						fullUrl := buildURL(site + config.Path, config.AvoidCache)
+						bodyRaw := getHttpBody(fullUrl)
+						extractedValue := extractValue(bodyRaw, config.RegexpString)
+						builtSingleResultLine := buildSingleResultLine(site, extractedValue, config.MetricString)
+
+						buf.WriteString(builtSingleResultLine + "\n")
+					}(site)
+				}
+				wg.Wait()
+
+				io.Copy(w,buf)
+			}),
+		},
+	}
+
+	log.Println("Server is now listening on ", config.ListenerAddress)
+	if err := server.ListenAndServe(); err != nil {
+		log.Fatalf("Listening server error: %s", err)
+	}
+}
